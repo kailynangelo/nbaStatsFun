eb9f6554526d2c80f981f732a5d9321ff1778821
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-09-24 15:49:44 -0500

    add User DAO class with getallusers method

---
+++ src/main/java/persistence/UserDao.java
@@ -0,0 +1,27 @@
+package persistence;
+
+import entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class UserDao {
+
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    public List<User> getAllUsers() {
+
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
+
+        return users;
+    }
+}
