c8b0d8ed1353cf70f14a94ce5372047e3f35378e
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-09-24 16:09:29 -0500

    add clean sql file

---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,5 @@
+DELETE FROM user;
+INSERT INTO user (user_name, password) VALUES ('admin', 'admin');
+INSERT INTO user (first_name, last_name, user_name, password) VALUES ('Kailyn', 'Angelo', 'kangelo', 'student');
+INSERT INTO user (first_name, last_name, user_name, password) VALUES ('Annie', 'Morgan', 'anniemo', 'student');
+INSERT INTO user (first_name, last_name, user_name, password) VALUES ('Stanley', 'Levenduski', 'stantheman', 'student');
