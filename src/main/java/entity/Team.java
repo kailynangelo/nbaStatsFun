fe08ea4af28a17bb2997a6533fc2e2fad19c63fd
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-12-05 14:29:09 -0600

    add annotations for Hibernate

--- src/main/java/entity/Team.java
+++ src/main/java/entity/Team.java
@@ -1,71 +1,163 @@
 package entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 import java.util.Objects;
 
+/**
+ * Object to store Team info.
+ */
+@Entity(name = "Team")
+@Table(name = "team")
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(name = "team_name")
     private String teamName;
+    @Column(name = "city")
     private String city;
+    @Column(name = "state")
     private String state;
+    @Column(name = "conference")
     private String conference;
+    @Column(name = "division")
     private String division;
+    @Column(name = "full_name")
     private String fullName;
 
+    /**
+     * Instantiates a new Team.
+     */
     public Team() {
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets team name.
+     *
+     * @return the team name
+     */
     public String getTeamName() {
         return teamName;
     }
 
+    /**
+     * Sets team name.
+     *
+     * @param teamName the team name
+     */
     public void setTeamName(String teamName) {
         this.teamName = teamName;
     }
 
+    /**
+     * Gets city.
+     *
+     * @return the city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Sets city.
+     *
+     * @param city the city
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Gets state.
+     *
+     * @return the state
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Sets state.
+     *
+     * @param state the state
+     */
     public void setState(String state) {
         this.state = state;
     }
 
+    /**
+     * Gets conference.
+     *
+     * @return the conference
+     */
     public String getConference() {
         return conference;
     }
 
+    /**
+     * Sets conference.
+     *
+     * @param conference the conference
+     */
     public void setConference(String conference) {
         this.conference = conference;
     }
 
+    /**
+     * Gets division.
+     *
+     * @return the division
+     */
     public String getDivision() {
         return division;
     }
 
+    /**
+     * Sets division.
+     *
+     * @param division the division
+     */
     public void setDivision(String division) {
         this.division = division;
     }
 
+    /**
+     * Gets full name.
+     *
+     * @return the full name
+     */
     public String getFullName() {
         return fullName;
     }
 
+    /**
+     * Sets full name.
+     *
+     * @param fullName the full name
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
