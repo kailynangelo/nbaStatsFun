80feaffb8fd50b77cc42c2301ca3ffd85d1ae76a
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-12-05 14:15:20 -0600

    add POJO for Team entity

---
+++ src/main/java/entity/Team.java
@@ -0,0 +1,97 @@
+package entity;
+
+import java.util.Objects;
+
+public class Team {
+    private int id;
+    private String teamName;
+    private String city;
+    private String state;
+    private String conference;
+    private String division;
+    private String fullName;
+
+    public Team() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getConference() {
+        return conference;
+    }
+
+    public void setConference(String conference) {
+        this.conference = conference;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "teamName='" + teamName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", conference='" + conference + '\'' +
+                ", division='" + division + '\'' +
+                ", fullName='" + fullName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Team)) return false;
+        Team team = (Team) o;
+        return Objects.equals(fullName, team.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fullName);
+    }
+}
