0f5f03a80b65e967f34445b8dbedbafcd0d4670d
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-12-09 13:35:13 -0600

    add teams to the session if teams attribute dne yet

--- src/main/java/controller/AllTeams.java
+++ src/main/java/controller/AllTeams.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import persistence.TeamDao;
 import persistence.UserDao;
 
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -20,12 +23,21 @@ import java.io.IOException;
 )
 
 public class AllTeams extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        TeamDao dao = new TeamDao();
-        req.setAttribute("teams", dao.getAllTeams());
-
+        HttpSession session = req.getSession();
+        if (session.getAttribute("teams") == null) {
+            TeamDao dao = new TeamDao();
+            //req.setAttribute("teams", dao.getAllTeams());
+            session.setAttribute("teams", dao.getAllTeams());
+            logger.debug("teams attribute was empty. added to the session." + dao.getAllTeams());
+        }
+        else {
+            logger.debug("teams already exists in the session. no need to add.");
+        }
+        logger.debug(session.getAttribute("teams"));
         RequestDispatcher dispatcher = req.getRequestDispatcher("/teams.jsp");
         dispatcher.forward(req, resp);
     }
